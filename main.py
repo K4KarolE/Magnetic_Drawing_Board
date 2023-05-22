30141d288dfef3a3fe29cb5bfcc514b60cfd5ed0
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-05-22 13:57:09 +0100

    cursor as an image

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pygame
+from pygame.locals import *
+import os
+
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
 pygame.init()
 
@@ -7,25 +13,45 @@ SCREEN_HEIGHT = 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Magnetic Drawing Board')
-screen.fill((255,255,255))
+
+# screen.set_colorkey(BLACK)
+# pygame.draw.circle(screen, (0,0,0,50), (50, 50), 30)
+
+#LOADING  IMAGE
+ball_image = pygame.image.load(os.path.join('pictures','skin', 'ball.png'))
 
 
 run = True
 while run:
 
+    # CURRENT CURSOR COORDINATES
+    x_cord, y_cord = pygame.mouse.get_pos()
+
     # DRAWING
     if pygame.mouse.get_pressed()[0] == True:
-        x_cord, y_cord = pygame.mouse.get_pos()
+        # x_cord, y_cord = pygame.mouse.get_pos()
         pygame.draw.circle(screen, (0,255,255,100), (x_cord, y_cord), 30)
 
     # ERASER
     if pygame.mouse.get_pressed()[2] == True:
         x_cord, y_cord = pygame.mouse.get_pos()
-        pygame.draw.circle(screen, (255,255,255,255), (x_cord, y_cord), 30)
-
+        pygame.draw.circle(screen, (255,255,100), (x_cord, y_cord), 30)
+        
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             run = False
     
-    pygame.display.flip()
+    # CURSOR AS AN IMAGE -> repainting the background -> drawing disappear
+    screen.fill(YELLOW)
+    img_width = ball_image.get_width()
+    img_height = ball_image.get_height()
+    cursor_position = pygame.mouse.get_pos()
+    cursor_with_image_position = (cursor_position[0]-img_width/2, cursor_position[1]-img_height/2)
+    screen.blit(ball_image, cursor_with_image_position)
+    
+    # Update the GUI pygame
+    pygame.display.update()
+ 
+# Quit the GUI game
+pygame.quit()
